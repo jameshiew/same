902b7d7d053093e329036de0dcb9caeb3f268054
James Hiew <james@hiew.net>
2020-02-20 22:22:28 +0000

    internal/branch: rename to internal/git

--- internal/git/branches.go
+++ internal/git/branches.go
@@ -1,4 +1,4 @@
-package branch
+package git
 
 import (
 	"context"
@@ -9,17 +9,17 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func get(ctx context.Context) ([]string, error) {
+func getBranchNames(ctx context.Context) ([]string, error) {
 	output, err := exec.CommandContext(ctx, "git", "for-each-ref", "refs/heads", "--format=%(refname:short)").Output()
 	if err != nil {
 		return nil, err
 	}
-	// TrimRight to prevent an empty branch name in the final slice of branch names
+	// TrimRight to prevent an empty git name in the final slice of git names
 	return strings.Split(strings.TrimRight(string(output), " \n"), "\n"), nil
 }
 
-func GetDifferent(ctx context.Context, from string) ([]string, error) {
-	brnchs, err := get(ctx)
+func GetDifferentBranchNames(ctx context.Context, from string) ([]string, error) {
+	brnchs, err := getBranchNames(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -28,16 +28,16 @@ func GetDifferent(ctx context.Context, from string) ([]string, error) {
 	var same []string
 	for _, brnch := range brnchs {
 		if brnch == from {
-			continue // don't include branch we are diffing against
+			continue // don't include git we are diffing against
 		}
 		err := exec.CommandContext(ctx, "git", "diff", from+".."+brnch, "--exit-code").Run()
 		if err != nil {
 			exitErr, ok := err.(*exec.ExitError)
 			if !ok {
-				return nil, fmt.Errorf("diffing branch %s: %v", brnch, err)
+				return nil, fmt.Errorf("diffing git %s: %v", brnch, err)
 			}
 			if exitErr.ExitCode() > 1 {
-				return nil, fmt.Errorf("diffing branch %s: output was %s - %v", brnch, exitErr.Stderr, exitErr)
+				return nil, fmt.Errorf("diffing git %s: output was %s - %v", brnch, exitErr.Stderr, exitErr)
 			}
 			log.Printf("Branch %s is different", brnch)
 			continue
